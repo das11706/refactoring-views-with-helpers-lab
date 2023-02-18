6f457f70546a1bca29faa62814b067c55884e400
Daniel Suarez <suarez.daniel101@gmail.com>
2023-02-17 22:47:06 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,11 +2,15 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   
   def artist_name
-    @artist_name
+    # puts self
+    # puts artist
+    self.artist.name
+    # binding.pry
   end
 
   def artist_name=(name)
-    @artist_name = name
+    new_artist = Artist.all.find_or_create_by(name: name)
+    self.artist = new_artist
   end
 
 end
